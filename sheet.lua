45ea9bd5278c4cbd5888cff5d20f1597ceb29b3f
IsTheWit <81376136+IsTheWit@users.noreply.github.com>
2022-04-15 16:54:54 -0500

    Create sheet.lua

---
+++ sheet.lua
@@ -0,0 +1,29 @@
+-- Sheet for Rondo Alla Turca by Wolfgang Amadeus Mozart
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[
+6 [80] 3 [80] - {3-$-^[6e]} [80] 3 [80] - [6u] u [80u] u [3u] u [80u] u [6u] u [80u] u [3u] u [80u] u [6u] u [80u] u [3u] i [80u] Y [6u]
+[80p] [3s] [80] [%d] d [90d] d [3d] s [80a] d [6s] s [80s] s [3s] a [80p] a [7a] a [(ea] a [$I] [(ea] [0WO] - [30] [6eu] u [80u] u [3u] u [80u]
+u [6u] u [80u] u [3u] u [80u] u [6u] i [80u] Y [3u] [80p] s [6f] [80k] [3l] [80] 9-z l [qek] z [8l] k [0ej] l [7k] j [0wH] k [6j] f-[80s]-a
+[30g] f [$Qd] s [%Wa] p [30O] a [6ep] - [30] [6eu] u [80u] u [3u] u [80u] u [6u] u [80u] u [3u] u [80u] u [6u] i [80u] Y [3u] [80a] [3l] [80]
+z [7z] [90z] z [3z] l [90k] z [6l] l [80l] l [3l] k [80j] l [7k] k [YIk] k [QG] [YIa] [0WO] - [30] [6u] u [80u] u [3u] u [80u] u [6u] u [80u] u
+[3u] u [80u] u [6u] u [80u] u [3u] i [80u] Y [6u] [80p] s [6f] [80k] [3l] [80] 9-z l [qek] z [8l] k [0ej] l [7k] j [0wH] k [6j] f-[80s]-a 7
+g-f-d-s-[3a]-p-O-a-[6p] - [30] [6ed] [qe] S [6d] [qef] [9g] [qef] [6d] [qeg] [8f] [0e] d [6s] [0ed] [8f] [0e] 6 [0es] [ua] [Wr] P [7a] [Wrs]
+[0d] [Wrs] [7a] [Wrd] [6s] [6p] [8s] [0f] [ej] [0f] [8s] [6p] [30] [6ed] [qe] S [6d] [qef] [9g] [qef] [6d] [qeg] [8f] [0e] d [6s] [0ed] [8f] [0e]
+[6j] [0e] 9 [qed] 7 [qeg] [30f] [38s] [39d] [37s] [36a] - [6eof] [ephkb] - [30] [6eu] u [80u] u [3u] u [80u] u [6u] u [80u] u [3u] u [80u]
+u [6u] u [80u] u [3u] i [80u] Y [6u] [80p] [3s] 3 [80] [7d] d [90d] d [3d] s [90a] d [6s] s [80s] s [3s] s a [80p] s [7s] a [(Qa] a a [$G] [(Qa] [0OH]
+- [30] [6u] u [80u] u [3u] u [80u] u [6u] u [80u] u [3u] u [80u] u [6u] u [80u] u [3u] i [80u] Y [6u] [80p] s [6f] [80k] [3l] [80] 9-z l [qek] z [8l] k [0ej] l [7k] j [0wH] k [6j] f-[80s]-a 7 g-f-d-s-[3a]-p-O-a-[6p] - [3e] [80] 3 [80] - {3-$-^[6e]} [80] 3 [80] [3e] [80e] 3 [80e] {3-$-^[6e]} [80e] 3 [80e]
+[30] [0et] 3 [0et] [30] [etu] 3 [etu] [30] [tup] 3 [ups] [30] [psf] 3 [sfj] [30f] [jl] [ps-f][80] 3 [ps-f] [ps-f][80] 6 [ps-f] [ps-f][80] 3
+[ps-f][80] 3 [ps-f] [ps-f][80] 6 [ps-f] [ps-f][80] [3f]-g-[80f] [6D] f [80j] [3l] [80] [7fH] z [90fH] z [3fH] z [90fH] z [fj] [6l] [80fj] l [3fj]
+[80fj] l [rDG] k [YIDG] k [QG] [YIk] [uOfH] - [30] [6f]-[ps]-[80f]-[ps]-[3f]-[ps]-[80f]-[ps]-[6f]-[ps]-[80f]-[ps]-[3f]-[ps]-[80f]-[ps]-[6f]-g-[80f] D [3f]
+[80j] k [6x] b [80] [3m] [80] [9z]-l-k [qe] z-l-[8k]-j [0e] l-k-[7j]-H[9W]-k-j-6-f-[8es]-p-7 g-f-d-s-[3a]-p-O-a-[6p] - 6 [30] [6ed] [qe] S [6d] [qef] [9g] [qef] [6d] [qeg] [8f] [0e] d [6s] [0ed] [8f] [0e] 6 [0es] [ua] [Wr] P [7a] [Wrs] [0d] [Wrs] [7a] [Wrd] [6s] [6p] [8s] [30f] [ej] [30f] [8s] [6p] [30]
+[6ed] [qe] S [6d] [qef] [9g] [qef] [6d] [qeg] [8f] [0e] d [6s] [0ed] [8f] [0e] [6j] [0e] 9 [qed] 7 [qeg] [30f] [38s] [39d] [37s] [36a] - [6eof] [ephkb] [30] [ps-f][80] 3 [ps-f] [ps-f][80] 6 [ps-f] [ps-f][80] 3
+[ps-f][80] 3 [ps-f] [ps-f][80] 6 [ps-f] [ps-f][80] [3f]-g-[80f] [6D] f [80j] [3l] [80] [7fH] z [90fH] z [3fH] z [90fH] z [fj] [6l] [80fj] l [3fj]
+[80fj] l [rDG] k [YIDG] k [QG] [YIk] [uOfH] - [30] [6f]-[ps]-[80f]-[ps]-[3f]-[ps]-[80f]-[ps]-[6f]-[ps]-[80f]-[ps]-[3f]-[ps]-[80f]-[ps]-[6f]-g-[80f] D [3f]
+[80j] k [6x] b [80] [3m] [80] [9z]-l-k [qe] z-l-[8k]-j [0e] l-k-[7j]-H[9W]-k-j-6-f-[8es]-p-7 g-f-d-s-[3a]-p-O-a-[6p]
+]]
